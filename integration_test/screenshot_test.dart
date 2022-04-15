32338a5f073657792ba64207029a79f3497ddfe4
jzx <945627077@qq.com>
2022-04-15 12:07:34 +0800

    '增加继承测试'

---
+++ integration_test/screenshot_test.dart
@@ -0,0 +1,7 @@
+import 'package:dino/main.dart' as app;
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  app.main();
+}
