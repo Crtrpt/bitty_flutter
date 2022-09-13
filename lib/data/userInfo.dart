86b6ed7fc98ce91bc2043595f3147a91f04daf26
jzx <945627077@qq.com>
2022-09-13 22:39:20 +0800

    完善类型注解

--- lib/data/userInfo.dart
+++
@@ -1,5 +0,0 @@
-userinfo() {
-  return {
-    "name": "",
-  };
-}
--- lib/page/model/session.dart
+++ lib/page/model/session.dart
@@ -3,31 +3,76 @@
 import 'package:dino/page/message/message.dart';
 
 class SessionMember {
-  String? user_id;
-  String? session_id;
-  int? session_type;
-  String? name;
-  String? avatar;
-  String? session_name;
-  String? session_avatar;
-  int? suspend;
-  int? attention;
-  int? type;
-  DateTime? created_at;
+  const SessionMember(
+      {this.user_id,
+      this.session_id,
+      this.session_type,
+      this.name,
+      this.avatar,
+      this.session_name,
+      this.session_avatar,
+      this.suspend,
+      this.attention,
+      this.type,
+      this.created_at});
+  final String? user_id;
+  final String? session_id;
+  final int? session_type;
+  final String? name;
+  final String? avatar;
+  final String? session_name;
+  final String? session_avatar;
+  final int? suspend;
+  final int? attention;
+  final int? type;
+  final DateTime? created_at;
+  factory SessionMember.fromJson(Map<String, dynamic> json) => SessionMember(
+        user_id: json['user_id'] as String,
+        session_id: json['session_id'] as String,
+        session_type: json['session_type'] as int,
+        name: json['name'] as String,
+        avatar: json['avatar'] as String,
+        session_name: json['session_name'] as String,
+        session_avatar: json['session_avatar'] as String,
+        suspend: json['suspend'] as int,
+        attention: json['attention'] as int,
+        type: json['type'] as int,
+        created_at: json['created_at'] as DateTime,
+      );
 }
 
 class Session {
-  String? name;
-  String? avatar;
-  String? session_id;
-  String? description;
-  int? type;
-  int? member_count;
-  DateTime? created_at;
+  const Session({
+    this.name,
+    this.avatar,
+    this.session_id,
+    this.description,
+    this.type,
+    this.member_count,
+    this.created_at,
+  });
+  final String? name;
+  final String? avatar;
+  final String? session_id;
+  final String? description;
+  final int? type;
+  final int? member_count;
+  final DateTime? created_at;
+  factory Session.fromJson(Map<String, dynamic> json) => Session(
+        name: json['name'] as String,
+      );
 }
 
 class SessionList {
-  List<Message>? chatList;
-  SessionMember? setting;
-  Session? session;
+  const SessionList({
+    this.session,
+    this.setting,
+    this.chatList,
+  });
+  final List<Message>? chatList;
+  final SessionMember? setting;
+  final Session? session;
+  factory SessionList.fronJson(Map<String, dynamic> json) => SessionList(
+      session: Session.fromJson(json['session']),
+      setting: SessionMember.fromJson(json['setting']));
 }
